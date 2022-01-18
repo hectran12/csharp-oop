a3ee4a34bd9c263508bb91b04ecb73da987c9e3d
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-01-18 22:20:04 +0700

    Add files via upload

---
+++ banhang/banhang/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+namespace banhang
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            cat cat = new cat();
+            Console.Write("Nhap so luong don hang muon dua vao kho: ");
+            int am = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string,string> kho = new Dictionary<string, string> ();
+            int az = 0;
+            for(int i = 0; i < am; i++)
+            {
+                az++;
+                Console.Write("Nhap ten don hang: ");
+                string catname = Convert.ToString(Console.ReadLine());
+                Console.Write("Nhap the loai don: ");
+                string type = Convert.ToString(Console.ReadLine());
+                Console.Write("Nhap so luong don: ");
+                int amz = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Nhap gia thanh sp: ");
+                int giathanh = Convert.ToInt32(Console.ReadLine());
+                cat.setItem(catname, type, amz, giathanh, az);
+                Dictionary<string, string> gop = cat.GetItem();
+                kho = gopdic(kho, gop, 1);
+            }
+            foreach (KeyValuePair<string, string> k in kho)
+            {
+                Console.WriteLine(k.Key+" : "+k.Value);
+            }
+        }
+        static Dictionary<string, string> gopdic (Dictionary<string, string> a1, Dictionary<string, string> a2,int type)
+        {
+            if (type == 1)
+            {
+                foreach (KeyValuePair<string, string> kv in a2)
+                {
+                    a1.Add(kv.Key, kv.Value);
+                }
+                return a1;
+            } else
+            {
+                foreach (KeyValuePair<String, string> kv in a1)
+                {
+                    a2.Add(kv.Key, kv.Value);
+                }
+                return a2;
+            }
+        }
+    }
+}
---
+++ banhang/banhang/cat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banhang
+{
+    internal class cat
+    {
+        #region gat
+        private string catName { get; set; }
+        private string catType { get; set; }
+        private int cost { get; set; }
+        private int amount { get; set; }
+        private int so { get; set; }
+        #endregion
+        #region prote
+
+        public void setItem ()
+        {
+            this.cost = 0;
+            this.amount = 0;
+            this.catName = "Null";
+            this.catType = "Null";
+        }
+        public void setItem (string cname, string ctype, int c,int sl, int so)
+        {
+            this.catName = cname;
+            this.catType = ctype;
+            this.cost = c;
+            this.amount = sl;
+            this.so = so;
+        }
+        #endregion
+        #region output
+        public Dictionary<string, string> GetItem()
+        {
+            Dictionary<string,string> dic = new Dictionary<string,string>();
+            dic.Add("Name ["+this.so+"]",this.catName);
+            dic.Add("Type ["+this.so+"]",this.catType);
+            dic.Add("Amount ["+this.so+"]",Convert.ToString(this.amount));
+            dic.Add("Cost [" + this.so + "]", Convert.ToString(this.cost));
+            return dic;
+        }
+        #endregion
+    }
+}
