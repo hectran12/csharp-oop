7b94e97238eaa487998bef23adcb1c853d0e51e2
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-01-23 15:51:00 +0700

    Add files via upload

---
+++ Quan Ly Sinh Vien/test/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Text;
+
+namespace test
+{
+    class Program 
+    {
+        static Student sinhvien = new Student();
+        static void Main (string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Menu();
+            }
+        }
+
+        static void Menu()
+        {
+            string menu;
+            menu = "[1] Them sinh vien\n";
+            menu += "[2] Quan ly sinh vien\n";
+            Console.Write(menu);
+            choose();
+        }
+
+        static void choose()
+        {
+            Console.Write("Nhap chuc nang: ");
+            int chucnang = Convert.ToInt32 (Console.ReadLine());
+            switch (chucnang)
+            {
+                case 1:
+                    addStudent();
+                    break;
+                case 2:
+                    ViewStudent();
+                    break;
+                default:
+                    Console.Write("Nhap sai roi, nhap lai di");
+                    Thread.Sleep(1000);
+                    break;
+            }
+        }
+
+        private static void ViewStudent()
+        {
+            while (true)
+            {
+                Console.Clear();
+                sinhvien.AllStudent();
+                int z = MenuView();
+                if(z == 1)
+                {
+                    break;
+                }
+            }
+
+        }
+
+        static int MenuView()
+        {
+            string menuview;
+            menuview = "[1] Sort\n";
+            menuview += "[2] Find student\n";
+            menuview += "[3] Find University\n";
+            menuview += "[4] Find Age\n";
+            menuview += "[5] Find Name\n";
+            menuview += "[6] Remove students\n";
+            menuview += "[7] Edit Students\n";
+            menuview += "[8] Exit\n";
+            Console.Write(menuview);
+            Console.Write("Nhap chuc nang: ");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            int exit = 0;
+            switch (ch)
+            {
+                case 1:
+                    Sort();
+                    break;
+                case 2:
+                    findstu();
+                    break;
+                case 3:
+                    finduvs();
+                    break;
+                case 4:
+                    findage();
+                    break;
+                case 5:
+                    findname();
+                    break;
+                case 6:
+                    rmstu();
+                    break;
+                case 7:
+                    edit();
+                    break;
+                case 8:
+                    exit = 1;
+                    break;
+                default:
+                    Console.Write("Khong co chuc nang nay trong menu!");
+                    break;
+            }
+            return exit;
+        }
+
+        private static void edit()
+        {
+            Console.Write("Nhap ma hoc sinh: ");
+            int ma = Convert.ToInt32(Console.ReadLine());
+            sinhvien.edit(ma);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+        }
+
+        private static void rmstu()
+        {
+            Console.Write("Nhap ma muon xoa: ");
+            int ma = Convert.ToInt32(Console.ReadLine());
+            sinhvien.rm(ma);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+
+        }
+
+        private static void findname()
+        {
+            Console.Write("Nhap ten muon tim: ");
+            string name = Convert.ToString(Console.ReadLine());
+            sinhvien.findname(name);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+        }
+
+        private static void findage()
+        {
+            Console.Write("Nhap so tuoi: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            sinhvien.findage(age);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+        }
+
+        private static void finduvs()
+        {
+            Console.Write("Nhap truong hoc can tim: ");
+            string uni = Convert.ToString(Console.ReadLine()).Trim();
+            sinhvien.finduni(uni);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+        }
+
+        private static void findstu()
+        {
+            Console.Write("Nhap ma sinh vien can tim: ");
+            int ma = Convert.ToInt32(Console.ReadLine());
+            sinhvien.findstu(ma);
+            Console.Write("Enter de back ve menu!");
+            Console.ReadLine();
+        }
+
+        private static void Sort()
+        {
+            Console.WriteLine("[1] Small -> large\n[2] Large -> small");
+            Console.Write("Nhap so: ");
+            int sort = Convert.ToInt32((Console.ReadLine()));
+            switch (sort)
+            {
+                case 1:
+                    sinhvien.sortlm(1);
+                    break;
+                case 2:
+                    sinhvien.sortlm(0);
+                    break;
+                default:
+                    Console.Write("Khong co chuc nang cu the!");
+                    Thread.Sleep(1000);
+                    break;
+            }
+            Console.Write("Enter de tiep tuc!");
+            Console.ReadLine();
+        }
+
+        private static void addStudent()
+        {
+            Console.Write("Nhap ten sinh vien: ");
+            string tenstudent = Convert.ToString(Console.ReadLine()).ToLower();
+            Console.Write("Nhap so tuoi cua ban: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap truong dai hoc: ");
+            string university = Convert.ToString(Console.ReadLine()).ToLower();
+            Console.Write("Nhap cong viec hien tai: ");
+            string job = Convert.ToString(Console.ReadLine()).ToLower();
+            sinhvien.name = tenstudent;
+            sinhvien.age = age;
+            sinhvien.university = university;
+            sinhvien.job = job;
+            Console.WriteLine(sinhvien.NewStudent());
+            Console.WriteLine(sinhvien.GenID());
+            Console.Write("Enter de back menu");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quan Ly Sinh Vien/test/Student.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string university { get; set; }
+        public string job { get; set; }
+        public string settype_sort { get; set; }
+        static Dictionary<int, List<string>> students = new Dictionary<int, List<string>>();
+        private List<string> newbie { get; set; }
+        public string NewStudent()
+        {
+            List<string> newhs = new List<string>();
+            newhs.Add(name);
+            newhs.Add(Convert.ToString(age));
+            newhs.Add(university);
+            newhs.Add(job);
+            newbie = newhs;
+            return "Khoi tao thanh cong info student";
+        }
+
+        public void AllStudent()
+        {
+            if(students.Count() == 0)
+            {
+                Console.WriteLine("Khong co hoc sinh nao het!");
+            }
+            else
+            {
+                int a = 0;
+                foreach (KeyValuePair<int, List<string>> hs in students)
+                {
+                    a++;
+                    Console.Write("STT: " + a + " | Ma: " + hs.Key + " | ");
+                    string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                    int tagsnum = 0;
+                    foreach (string item in hs.Value)
+                    {
+                        Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                        tagsnum++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        
+        }
+        public void edit(int ma)
+        {
+            if (students.ContainsKey(ma))
+            {
+                string ten = students[ma][0];
+                string age = students[ma][1];
+                string uni = students[ma][2];
+                string job = students[ma][3];
+                Console.Write("Nhap ten moi (" + ten + "): ");
+                students[ma][0] = Convert.ToString(Console.ReadLine());
+                Console.Write("Nhap age moi (" + age + "): ");
+                students[ma][1] = Convert.ToString(Console.ReadLine());
+                Console.Write("Nhap university moi (" + uni + "): ");
+                students[ma][2] = Convert.ToString(Console.ReadLine());
+                Console.Write("Nhap job moi (" + job + "): ");
+                students[ma][3] = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Doi thong tin thanh cong!");
+            } else
+            {
+                Console.WriteLine("Khong co hoc sinh can tim!");
+            }
+        }
+        public void rm (int ma)
+        {
+            if (students.ContainsKey(ma))
+            {
+                Console.WriteLine("Xoa thanh cong hoc sinh " + students[ma][0]);
+                students.Remove(ma);
+            } else
+            {
+                Console.WriteLine("Khong tim thay hoc sinh can xoa!");
+            }
+
+        }
+        public void findstu (int ma)
+        {
+            if (!students.ContainsKey(ma))
+            {
+                Console.WriteLine("Khong co hoc sinh can tim!");
+            } else
+            {
+                Console.Write("Ma: " + ma + " | ");
+                string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                int tagsnum = 0;
+                foreach (string item in students[ma])
+                {
+                    Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                    tagsnum++;
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+        public void sortlm(int sr)
+        {
+            if (students.Count() == 0)
+            {
+                Console.WriteLine("Khong co sinh vien nao de lam viec nay!");
+            }
+            else
+            {
+                List<int> num = new List<int>();
+                foreach (KeyValuePair<int, List<string>> hs in students)
+                {
+                    if (num.Contains(Convert.ToInt32(hs.Value[1])) == false)
+                    {
+                        num.Add(Convert.ToInt32(hs.Value[1]));
+                    }
+
+                }
+                num.Sort();
+                if (sr == 0)
+                {
+                    num.Reverse();
+                }
+
+                Dictionary<int, List<string>> liststudent = new Dictionary<int, List<string>>();
+                foreach (int number in num)
+                {
+                    foreach (KeyValuePair<int, List<string>> hs in students)
+                    {
+                        if (Convert.ToInt32(hs.Value[1]) == number)
+                        {
+                            liststudent.Add(hs.Key, hs.Value);
+                        }
+                    }
+                }
+
+
+                int a = 0;
+                foreach (KeyValuePair<int, List<string>> hs in liststudent)
+                {
+                    a++;
+                    Console.Write("STT: " + a + " | Ma: " + hs.Key + " | ");
+                    string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                    int tagsnum = 0;
+                    foreach (string item in hs.Value)
+                    {
+                        Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                        tagsnum++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+           
+        }
+        public void findname (string name)
+        {
+            Dictionary<int, List<string>> liststudent = new Dictionary<int, List<string>>();
+
+            foreach (KeyValuePair<int, List<string>> hs in students)
+            {
+                if (hs.Value[0].IndexOf(name.ToLower()) > 0)
+                {
+                    liststudent.Add(hs.Key, hs.Value);
+                }
+            }
+            if (liststudent.Count > 0)
+            {
+                int a = 0;
+                foreach (KeyValuePair<int, List<string>> hs in liststudent)
+                {
+                    a++;
+                    Console.Write("STT: " + a + " | Ma: " + hs.Key + " | ");
+                    string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                    int tagsnum = 0;
+                    foreach (string item in hs.Value)
+                    {
+                        Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                        tagsnum++;
+                    }
+                    Console.WriteLine();
+                }
+            } else
+            {
+                Console.WriteLine("Khong co ten hoc sinh trong ds");
+            }
+  
+        }
+        public void findage(int number)
+        {
+            Dictionary<int, List<string>> liststudent = new Dictionary<int, List<string>>();
+           
+            foreach(KeyValuePair<int,List<string>> hs in students)
+            {
+                if (Convert.ToInt32(hs.Value[1]) == number)
+                {
+                    liststudent.Add(hs.Key,hs.Value);
+                }
+            }
+            if(liststudent.Count > 0)
+            {
+                int a = 0;
+                foreach (KeyValuePair<int, List<string>> hs in liststudent)
+                {
+                    a++;
+                    Console.Write("STT: " + a + " | Ma: " + hs.Key + " | ");
+                    string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                    int tagsnum = 0;
+                    foreach (string item in hs.Value)
+                    {
+                        Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                        tagsnum++;
+                    }
+                    Console.WriteLine();
+                }
+            } else
+            {
+                Console.WriteLine("Khong co so tuoi nay trong ds") ;
+            }
+            
+        }
+        public void finduni (string uni)
+        {
+            Dictionary<int, List<string>> liststudent = new Dictionary<int, List<string>>();
+
+            foreach (KeyValuePair<int, List<string>> hs in students)
+            {
+              //  Console.WriteLine(hs.Value[2]);
+                if (hs.Value[2] == uni.ToLower())
+                {
+                    liststudent.Add(hs.Key, hs.Value);
+                }
+            }
+            if(liststudent.Count > 0)
+            {
+                int a = 0;
+                foreach (KeyValuePair<int, List<string>> hs in liststudent)
+                {
+                    a++;
+                    Console.Write("STT: " + a + " | Ma: " + hs.Key + " | ");
+                    string[] tags = { "HoVaTen", "Age", "University", "Job" };
+                    int tagsnum = 0;
+                    foreach (string item in hs.Value)
+                    {
+                        Console.Write(tags[tagsnum] + ": " + ToUU(item) + " | ");
+                        tagsnum++;
+                    }
+                    Console.WriteLine();
+                }
+            } else
+            {
+                Console.WriteLine("Khong tim thay truong hoc");
+            }
+
+        }
+        private string ToUU(string ten)
+        {
+            string newt1 = ten.Trim();
+            string[] newt2 = newt1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string newten = "";
+            foreach (string pt in newt2)
+            {
+                string ptz = Convert.ToString(pt);
+                ptz = ptz.ToLower();
+                char[] giaikitu = ptz.ToCharArray();
+                giaikitu[0] = char.ToUpper(giaikitu[0]);
+                string newchar = new string(giaikitu);
+                newten += newchar + " ";
+            }
+            string tenmoi = newten.Trim();
+            return tenmoi;
+        }
+        public string GenID()
+        {
+            Random ran = new Random();
+            while (true)
+            {
+                int ranew = ran.Next(10000000,99999999);
+                if (students.ContainsKey(ranew) == false)
+                { 
+                    students.Add(ranew,newbie);
+                    return "Khoi tao id cho hoc sinh " + name;
+                    break;
+                } else
+                {
+                    return "Khoi tao khong thanh cong";
+                }
+            }
+
+        }
+    }
+}
